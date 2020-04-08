45ad98a6c20d4b0aac1e2e35069a99693a0315bf
amathsow <amathsow88@outlook.fr>
2020-04-08 19:42:56 +0000

    Create middle_of_linked_list.py
    Given a non-empty, singly linked list with head node head, return a middle node of linked list.
    
    If there are two middle nodes, return the second middle node.
    
     
    
    Example 1:
    
    Input: [1,2,3,4,5]
    Output: Node 3 from this list (Serialization: [3,4,5])
    The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
    Note that we returned a ListNode object ans, such that:
    ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
    Example 2:
    
    Input: [1,2,3,4,5,6]
    Output: Node 4 from this list (Serialization: [4,5,6])
    Since the list has two middle nodes with values 3 and 4, we return the second one.
     
    
    Note:
    
    The number of nodes in the given list will be between 1 and 100.

---
+++ middle_of_linked_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        list = []
+        while head:
+            list.append(head)
+            head = head.next
+        return list[len(list)/2]
